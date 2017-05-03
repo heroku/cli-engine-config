1b1e44da3ef0f765f3da90ac1792603fe48dadda
Ransom Briggs <rbriggs@heroku.com>
2017-05-03 16:24:52 -0500

    Add install uuid to user config

--- src/user-config.test.js
+++ src/user-config.test.js
@@ -4,7 +4,8 @@ import { type UserConfig } from './user-config'
 
 describe('UserConfig', () => {
   test('has a skipAnalytics flag', () => {
-    const userConfig: UserConfig = {skipAnalytics: true}
+    const userConfig: UserConfig = {skipAnalytics: true, install: '1234'}
     expect(userConfig).toHaveProperty('skipAnalytics', true)
+    expect(userConfig).toHaveProperty('install', '1234')
   })
 })
