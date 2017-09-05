d2e687d9f659defeb56bbc322572911750736888
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-05 13:05:08 -0700

    test

--- src/config.js
+++ src/config.js
@@ -90,7 +90,6 @@ function dir (config: Config, category: string, d: ?string): string {
 
 function debug (bin: string) {
   const debug = (process.env.DEBUG || '').includes('*') || envVarTrue(envVarKey(bin, 'DEBUG'))
-
   return debug ? 1 : 0
 }
 
