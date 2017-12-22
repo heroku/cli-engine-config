cb62e00f1a8bf9203fa5aa93cf15bd4fd780caa7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-22 14:55:54 -0800

    updated deps

--- src/config.ts
+++ src/config.ts
@@ -229,7 +229,7 @@ export interface ICommand {
   buildHelpLine: (config: IConfig) => [string, string | undefined]
   args?: args.IArg[]
   flags?: flags.Input
-  run: (argv: string[], options: IConfig) => Promise<any>
+  run: (argv: string[], config: IConfig) => Promise<any>
   plugin?: IPlugin
 }
 
